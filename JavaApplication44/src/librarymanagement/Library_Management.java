dcd7d5ef31d6bc2f460d31d64fce5e5e5cb4513c
cassedre <henriquedps2008@hotmail.com>
2021-11-05 16:32:01 +0000

    Package name changed

---
+++ JavaApplication44/src/librarymanagement/Library_Management.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package librarymanagement;
+
+/**
+ *
+ * @author 35383
+ */
+public class Library_Management {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("hello world");
+        System.out.println("hello world");
+        System.out.println("hello world");
+    }
+    
+}
